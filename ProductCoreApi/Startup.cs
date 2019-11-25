d4b61f4b7e615bd7d72a1b744a9cd4edeb491af6
hp <hp@DESKTOP-0PA6LIA>
2019-11-25 10:17:28 +0530

    srj

--- ProductCoreApi/Startup.cs
+++ ProductCoreApi/Startup.cs
@@ -36,10 +36,10 @@ namespace ProductCoreApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
